3f83335b26235a0a6580d0d178df8ecc56d60b02
U-rex13 <udhavnpatil9999@gmail.com>
2022-09-07 19:01:52 +0530

    login id and password with 3 attempts

---
+++ Password_validator.py
@@ -0,0 +1,38 @@
+import csv 
+def main():
+    with open("password.txt","r") as file:
+        file_reader = csv.reader(file)
+        user_info(file_reader)
+        file.close() 
+        
+def user_info(file):
+    user = input("Enter your username: ")
+    for row in file:
+        if row[0] != user:
+            pass
+        elif row[0] == user:
+            print("Helllo",row[0])
+            user_found=[row[0], row[1]]
+            pass_check(user_found)
+            break
+        else:
+            print("not found")
+            continue
+
+def pass_check(user_found):
+    attempts = 0
+    max_attempts = 2
+    while True:
+        user = input("enter your password: ")
+        if user_found[1] == user:
+            for i in range(3):
+                print("password match")
+            break
+        else:
+            print("password not match")
+            attempts += 1
+        if attempts > max_attempts:
+            print("Invalid password")
+            break            
+main()
+            
